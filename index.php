e8d041d927071626cebf590f65efb309c4b2cc70
celhryn <79858410+celhryn@users.noreply.github.com>
2022-04-08 08:51:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'Dudul8585!');
+$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'root');
 if(isset($_POST['envoyer'])){
     if(!empty($_POST['pseudonyme']) AND !empty($_POST['message']) ){
         $pseudo = htmlspecialchars($_POST['pseudonyme']);
