205e4607b60e7db1794146e80b96d96c2c15d127
celhryn <79858410+celhryn@users.noreply.github.com>
2022-04-08 08:50:57 +0200

    Update chargementMessage.php

--- chargementMessage.php
+++ chargementMessage.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'Dudul8585!');
+$bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'root');
 $recupMessages = $bdd->query('SELECT * FROM messages');
 while($message = $recupMessages->fetch()){
     ?>
