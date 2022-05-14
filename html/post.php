a351feb73e340e1d6090ce343777c019ef9db386
Tutur0747 <74667934+arthur0747@users.noreply.github.com>
2022-05-14 18:16:25 +0200

    Add files via upload

---
+++ html/post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Messagerie v0.1</title>
+        <meta charset='utf-8'>
+        <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <center>
+            <form method="POST" action="">
+                <input type="text" name="pseudo" placeholder="pseudo"><br><br>
+                <input type="text" name="message" placeholder="message"><br><br>
+                <input type="submit" name="envoyer">
+            </form>
+        </center>
+    </body>
+</html>
