b2ee77fcaddf2cc4f9ba3780cd5cb8ddd0fce0d1
celhryn <79858410+celhryn@users.noreply.github.com>
2022-05-15 15:03:17 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include('html/post.php');
 
-if(isset($_POST['valider'])){
-    include('php/post.php');}
-
+if(isset($_POST['pseudo']) AND isset($_POST['message'])){
+include('php/post.php');}
 ?>
