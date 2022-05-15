5eddd5c456ff6c8409602c9d4a7d81dfc01dd4d6
celhryn <79858410+celhryn@users.noreply.github.com>
2022-05-15 15:02:58 +0200

    Add files via upload

--- php/post.php
+++ php/post.php
@@ -1,24 +1,21 @@
 <?php
 include('config/config.php');
-include('html/post.php');
-include('index.php');
 
 $pseudo = htmlspecialchars($_POST['pseudo']);
 $message = nl2br(htmlspecialchars($_POST['message']));
 
 if(isset($_POST['valider'])){
-    if(empty($pseudo) AND empty($message))
-    {
-    echo("Veuillez remplir les deux champs !");
-    }
-    if(empty($pseudo) OR empty($message))
-    {
-        echo("Le champ pseudo ou le champ message n'est pas rempli !");
-    }
-    if(!empty($pseudo) AND !empty($message))
-    {
-        echo("Le message a été envoyé avec succès !");
-        $insertMessage = $bdd->prepare("INSERT INTO messages('', pseudonyme, message) VALUES(?, ?)");
-        $insertMessage->execute(array($pseudo, $message));
-    }
+if(empty($pseudo) AND empty($message))
+{
+echo("Veuillez remplir les deux champs !");
+}
+if(empty($pseudo) OR empty($message))
+{
+    echo("Le champ pseudo ou le champ message n'est pas rempli !");
+}
+if(!empty($pseudo) AND !empty($message))
+{
+    echo("Le message a été envoyé avec succès !");
+    $insertMessage = $bdd->prepare("INSERT INTO messages('', pseudo, message) VALUES(NULL, ?, ?)");
+    $insertMessage->execute(array($pseudo, $message));
 }
