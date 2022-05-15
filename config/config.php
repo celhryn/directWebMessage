0909c77ddd3cdd3c544fc13a4e40b2fb71bdcf0e
celhryn <79858410+celhryn@users.noreply.github.com>
2022-05-15 16:56:59 +0200

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,3 +1,9 @@
 <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'root');
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=messagerie;charset=utf8', 'root', 'Dudul8585!');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 ?>
