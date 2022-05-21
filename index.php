3dd8add2283cecfa5856bb4fea3753035effa0e5
celhryn <79858410+celhryn@users.noreply.github.com>
2022-05-21 11:55:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-include('html/form.php');
+include('html/post.php');
 include('config/config.php');
 
 if(isset($_POST['pseudo']) AND isset($_POST['message'])){
-include('php/post.php');}
+
+        include('php/post.php');
+}
 ?>
+
+<?php $recupMessages = $bdd->query('SELECT * FROM messages'); ?>
+
+<div class="message">
+        <br><br>
+        <b><?php echo($pseudo); ?></b>
+        <p><?php echo($message); ?></p>
+        <br><br>
+    </div>
